c32724fab3996cec9493c37235f260d7c4007ef5
losifer <carloscanash@gmail.com>
2015-11-04 15:24:00 -0800

    Updates 3:25pm
    Added User Storyboard
    Brought in View Controllers

---
+++ GiveABO/UserDonationCountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserDonationCountViewController.swift
+//  GiveABO
+//
+//  Created by Sebastian Drew on 11/4/15.
+//  Copyright © 2015 los. All rights reserved.
+//
+
+import UIKit
+
+class UserDonationCountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
