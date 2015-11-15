0cffdcdd02c7b6af69d94b9364e4adf0ea4b4d13
losifer <carloscanash@gmail.com>
2015-11-14 22:34:02 -0800

    AutosParse autouser removed
    Beginning to connect new user creation

--- GiveABO/AppDelegate.swift
+++ GiveABO/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Enable storing and querying data from Local Datastore.
         // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
-        Parse.enableLocalDatastore()
+        // Parse.enableLocalDatastore()
         
         // ****************************************************************************
         // Uncomment this line if you want to enable Crash Reporting
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // PFFacebookUtils.initializeFacebook()
         // ****************************************************************************
         
-        PFUser.enableAutomaticUser()
+        //PFUser.enableAutomaticUser()
         
         let defaultACL = PFACL();
         
