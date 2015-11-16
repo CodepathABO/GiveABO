fa9494ef228a310c34683b9e62a5a50885b56bbe
christophersybico <christophersybico@gmail.com>
2015-11-15 18:15:10 -0800

    Added filter dismiss button

--- GiveABO/FilterViewController.swift
+++ GiveABO/FilterViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
+    @IBOutlet weak var filterContentView: UIView!
    
+    @IBOutlet weak var filterDoneView: UIView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        filterContentView.alpha = 0
+        filterContentView.transform = CGAffineTransformMakeTranslation(0, 40)
+        
+        filterDoneView.alpha = 0
+        filterDoneView.transform = CGAffineTransformMakeTranslation(0, 40)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        
+        // ANIMATE FILTER VIEW
+        
+        UIView.animateWithDuration(
+            
+            0.8,
+            delay: 0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0.2,
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            
+            animations: { () -> Void in
+                self.filterContentView.alpha = 1
+                self.filterContentView.transform = CGAffineTransformMakeTranslation(0, 0)
+            }
+        )
+        {
+            (finished: Bool) -> Void in
+                
+        }
+        
+        
+        // ANIMATE DONE VIEW
+        
+        UIView.animateWithDuration(
+            
+            0.8,
+            delay: 0.4,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0.2,
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            
+            animations: { () -> Void in
+                self.filterDoneView.alpha = 1
+                self.filterDoneView.transform = CGAffineTransformMakeTranslation(0, 0)
+            }
+            )
+            {
+                (finished: Bool) -> Void in
+                
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,16 +79,8 @@ class FilterViewController: UIViewController {
     @IBAction func didPressBloodType(sender: UIButton) {
         sender.selected = !sender.selected
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didPressDone(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
 }
