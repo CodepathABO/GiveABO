ae5c1b3be6eac7cff573b0399b8ba6babf0d18d5
christophersybico <christophersybico@gmail.com>
2015-11-15 17:56:24 -0800

    Added Filter Screen

---
+++ GiveABO/FilterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FilterViewController.swift
+//  GiveABO-3
+//
+//  Created by Sybico, Christopher on 11/15/15.
+//  Copyright © 2015 los. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func didPressBloodType(sender: UIButton) {
+        sender.selected = !sender.selected
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
